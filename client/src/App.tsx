e9df53588803ba9fe3e1e1e079c4752c6d497d04
Nihar Kadam <nihar.kadam@gmail.com>
2026-03-03 22:44:41 +0530

    Minor UI changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -81,12 +81,19 @@ function App() {
           value={url}
           onChange={(e) => setUrl(e.target.value)}
           onKeyDown={(e) => e.key === "Enter" && handleSubmit()}
-          className={`w-full border rounded-lg px-3 py-2 mb-2 focus:outline-none focus:ring-2 transition
-            ${
-              url && !isValidUrl(url)
-                ? "border-red-500 focus:ring-red-500"
-                : "focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700"
-            }`}
+          className={`
+          w-full px-3 py-2 mb-2 rounded-lg border
+          bg-white text-black
+          dark:bg-gray-800 dark:text-white
+          dark:border-gray-700
+          focus:outline-none focus:ring-2
+          transition-colors duration-200
+          ${
+            url && !isValidUrl(url)
+              ? "border-red-500 focus:ring-red-500"
+              : "border-gray-300 focus:ring-blue-500"
+          }
+        `}
         />
         {url && !isValidUrl(url) && (
           <p className="text-red-500 text-sm mb-3">Invalid URL format</p>
@@ -123,7 +130,7 @@ function App() {
               />
               <button
                 onClick={copyToClipboard}
-                className="bg-gray-200 dark:bg-gray-700 text-gray-900 dark:text-gray-100px-3 rounded-lg hover:bg-gray-300 dark:hover:bg-gray-600 transition"
+                className="bg-gray-200 dark:bg-gray-700 text-gray-900 dark:text-gray-100 px-3 rounded-lg hover:bg-gray-300 dark:hover:bg-gray-600 transition"
               >
                 {copied ? (
                   <span className="text-green-500 animate-bounce">✔</span>
