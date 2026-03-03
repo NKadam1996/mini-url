1b400752c4822a16165bd8ad6baa14f32d11f23e
Nihar Kadam <nihar.kadam@gmail.com>
2026-03-03 17:14:04 +0530

    Updated Redis config

--- server/src/config/redis.ts
+++ server/src/config/redis.ts
@@ -1,7 +1,10 @@
 import Redis from "ioredis";
 
-export const redis = new Redis({
-  host: process.env.REDIS_HOST || "redis",
-  port: Number(process.env.REDIS_PORT) || 6379,
-  maxRetriesPerRequest: 3,
-});
+const redis = process.env.REDIS_URL
+  ? new Redis(process.env.REDIS_URL)
+  : new Redis({
+      host: "redis",
+      port: 6379,
+    });
+
+export default redis;
